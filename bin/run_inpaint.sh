1038db156e0e1fb9736078cf64dc8e74b5ae60ad
haokai-jojo <haokai@biomap.com>
2026-01-24 22:51:12 +0800

    add prediction script

--- bin/run_inpaint.sh
+++ bin/run_inpaint.sh
@@ -1,5 +1,24 @@
-# Run inpaint
-python scripts/predict.py --data data/structure/inpaint --processed_msa_dir data/msa
+#!/bin/bash
+#SBATCH --chdir=/hai/scratch/fangwu97/mfdesign
+#SBATCH --cpus-per-task=30        # Number of CPU cores per task
+#SBATCH --mem=628G                # Total memory per node (or job), e.g. 16G, 3200M
+#SBATCH --time=1-00:00:00       # 3 days
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gres=gpu:1
+#SBATCH --account=yejin
 
-# Run fold
-python scripts/predict.py --data data/structure/fold --processed_msa_dir data/msa
+CONDA=/hai/scratch/fangwu97/miniconda3
+
+source $CONDA/bin/activate
+
+conda activate $CONDA/envs/mfdesign
+
+export PYTHONPATH=$CONDA/envs/mfdesign/bin/python
+
+data_dir=$1
+
+# data/structure/inpaint
+# data/structure/fold
+# Run inpaint
+python scripts/predict.py --data ${data_dir} --processed_msa_dir data/msa
