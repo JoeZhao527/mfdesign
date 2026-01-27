4202e775e7ed8f396457d750686ee631629a4456
haokai-jojo <haokai@biomap.com>
2026-01-27 21:41:38 +0800

    eval boltz

--- bin/run_eval.sh
+++ bin/run_eval.sh
@@ -1,17 +1,43 @@
-python evaluate/AbX_eval/eval_metric.py \
-    --data_dir boltz_results_fold/predictions \
-    --ref_dir ./data/test_entry_pdb_files \
-    --test_yaml_dir data/test_yaml_dir/ab \
-    -test_json_fpath data/test_entry.json \
-    --model boltz \
-    --suffix _model_0.pdb \
-    -c 8 
+#!/bin/bash
 
-python evaluate/AbX_eval/eval_metric.py \
-    --data_dir boltz_results_inpaint/predictions \
-    --ref_dir ./data/test_entry_pdb_files \
-    --test_yaml_dir data/test_yaml_dir/ab \
-    -test_json_fpath data/test_entry.json \
-    --model boltz \
-    --suffix _model_0.pdb \
-    -c 8 
+# Fold results
+FOLD_DIRS=(
+    "boltz_results_boltz_all_mask_no_playback_0"
+    "boltz_results_boltz_all_mask_no_playback_1"
+    "boltz_results_boltz_all_mask_playback_0"
+    "boltz_results_boltz_all_mask_playback_1"
+)
+
+# Inpaint results
+INPAINT_DIRS=(
+    "boltz_results_boltz_predicted_no_playback_0"
+    "boltz_results_boltz_predicted_no_playback_1"
+    "boltz_results_boltz_predicted_playback_0"
+    "boltz_results_boltz_predicted_playback_1"
+)
+
+# Evaluate fold results
+for dir in "${FOLD_DIRS[@]}"; do
+    echo "Evaluating fold: $dir"
+    python evaluate/AbX_eval/eval_metric.py \
+        --data_dir output/fold/$dir/predictions \
+        --ref_dir ./data/test_entry_pdb_files \
+        --test_yaml_dir data/test_yaml_dir/ab \
+        --test_json_fpath data/test_entry.json \
+        --model boltz \
+        --suffix _model_0.pdb \
+        -c 20
+done
+
+# Evaluate inpaint results
+for dir in "${INPAINT_DIRS[@]}"; do
+    echo "Evaluating inpaint: $dir"
+    python evaluate/AbX_eval/eval_metric.py \
+        --data_dir output/inpaint/$dir/predictions \
+        --ref_dir ./data/test_entry_pdb_files \
+        --test_yaml_dir data/test_yaml_dir/ab \
+        --test_json_fpath data/test_entry.json \
+        --model boltz \
+        --suffix _model_0.pdb \
+        -c 20
+done
