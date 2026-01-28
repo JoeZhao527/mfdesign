6e9a9f770b652edc9e4dc123ac126415944f9685
haokai-jojo <haokai@biomap.com>
2026-01-28 16:25:25 +0800

    enable pyrosetta

--- bin/cal_interface.sh
+++ bin/cal_interface.sh
@@ -1,5 +1,19 @@
 python evaluate/AbX_eval/cal_interface_energy.py \
+    --pdb_dir relaxed_out/inpaint/boltz_results_boltz_predicted_playback_0/predictions \
+    --output_dir binding_res/pb_inpaint_0 \
+    --cpus 30
+
+python evaluate/AbX_eval/cal_interface_energy.py \
     --pdb_dir relaxed_out/inpaint/boltz_results_boltz_predicted_playback_1/predictions \
     --output_dir binding_res/pb_inpaint_1 \
-    --cpus 1 \
-    --verbose true
+    --cpus 30
+
+python evaluate/AbX_eval/cal_interface_energy.py \
+    --pdb_dir relaxed_out/inpaint/boltz_results_boltz_predicted_no_playback_0/predictions \
+    --output_dir binding_res/no_pb_inpaint_0 \
+    --cpus 30
+
+python evaluate/AbX_eval/cal_interface_energy.py \
+    --pdb_dir relaxed_out/inpaint/boltz_results_boltz_predicted_no_playback_1/predictions \
+    --output_dir binding_res/no_pb_inpaint_1 \
+    --cpus 30
