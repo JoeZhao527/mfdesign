b85464445315588a471aeaff336dc99cfb339c87
haokai-jojo <haokai@biomap.com>
2026-01-28 20:02:19 +0800

    adding final results

--- bin/launch_inference.sh
+++ bin/launch_inference.sh
@@ -1,3 +1,19 @@
+final_result_tools/output/boltz_all_mask_trail_0
+final_result_tools/output/boltz_all_mask_trail_1
+final_result_tools/output/boltz_predicted_trail_0
+final_result_tools/output/boltz_predicted_trail_1
+
+
+sbatch -p yejin --output=logs/trail_0_inpaint.log bin/run_inpaint.sh final_result_tools/output/boltz_predicted_trail_0
+sbatch -p yejin --output=logs/trail_1_inpaint.log bin/run_inpaint.sh final_result_tools/output/boltz_predicted_trail_1
+
+
+
+sbatch -p yejin --output=logs/trail_0_fold.log bin/run_fold.sh final_result_tools/output/boltz_all_mask_trail_0
+
+
+
+
 sbatch -p yejin --output=logs/back_0_fold.log bin/run_fold.sh data/boltz_all_mask_playback_0
 sbatch -p yejin --output=logs/back_1_fold.log bin/run_fold.sh data/boltz_all_mask_playback_1
 sbatch -p yejin --output=logs/no_back_0_fold.log bin/run_fold.sh data/boltz_all_mask_no_playback_0
--- bin/run_inpaint.sh
+++ bin/run_inpaint.sh
@@ -2,8 +2,8 @@
 #SBATCH --job-name=inpaint
 #SBATCH --output=logs/inpaint.log
 #SBATCH --chdir=/hai/scratch/fangwu97/mfdesign
-#SBATCH --cpus-per-task=30        # Number of CPU cores per task
-#SBATCH --mem=628G                # Total memory per node (or job), e.g. 16G, 3200M
+#SBATCH --cpus-per-task=4        # Number of CPU cores per task
+#SBATCH --mem=100G                # Total memory per node (or job), e.g. 16G, 3200M
 #SBATCH --time=1-00:00:00       # 3 days
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
@@ -24,7 +24,7 @@ DATA_DIR=$1
 # Run inpaint
 python scripts/predict.py \
     --data $DATA_DIR \
-    --out_dir ./native_output/inpaint \
+    --out_dir ./0128_out/inpaint \
     --processed_msa_dir data/msa \
     --checkpoint ./model/boltz1.ckpt \
     --only_structure_prediction \
