0e64ada0b9661c95eb912f9dd98cbe98a6138e9f
haokai-jojo <haokai@biomap.com>
2026-01-29 16:48:56 +0800

    grid search mpnn

--- AntiBMPNN/launch.sh
+++ AntiBMPNN/launch.sh
@@ -1,8 +1,8 @@
-sbatch run_antibmpnn_batch.sh --backbone_noise 0.1 --pdb_suffix _model_0.pdb --sampling_temp 0.2
-sbatch run_antibmpnn_batch.sh --backbone_noise 0.1 --pdb_suffix _model_0.pdb --sampling_temp 0.4
-sbatch run_antibmpnn_batch.sh --backbone_noise 0.1 --relaxed --sampling_temp 0.2
-sbatch run_antibmpnn_batch.sh --backbone_noise 0.1 --relaxed --sampling_temp 0.4
-sbatch run_antibmpnn_batch.sh --backbone_noise 0.5 --pdb_suffix _model_0.pdb --sampling_temp 0.2
-sbatch run_antibmpnn_batch.sh --backbone_noise 0.5 --pdb_suffix _model_0.pdb --sampling_temp 0.4
-sbatch run_antibmpnn_batch.sh --backbone_noise 0.5 --relaxed --sampling_temp 0.2
-sbatch run_antibmpnn_batch.sh --backbone_noise 0.5 --relaxed --sampling_temp 0.4
+sbatch -p yejin run_antibmpnn_batch.sh --backbone_noise 0.1 --pdb_suffix _model_0.pdb --sampling_temp 0.2 --output_dir ./antibmpnn_output/bn0.1_raw_t0.2
+sbatch -p yejin run_antibmpnn_batch.sh --backbone_noise 0.1 --pdb_suffix _model_0.pdb --sampling_temp 0.4 --output_dir ./antibmpnn_output/bn0.1_raw_t0.4
+sbatch -p yejin run_antibmpnn_batch.sh --backbone_noise 0.1 --relaxed --sampling_temp 0.2 --output_dir ./antibmpnn_output/bn0.1_relaxed_t0.2
+sbatch -p yejin run_antibmpnn_batch.sh --backbone_noise 0.1 --relaxed --sampling_temp 0.4 --output_dir ./antibmpnn_output/bn0.1_relaxed_t0.4
+sbatch -p yejin run_antibmpnn_batch.sh --backbone_noise 0.5 --pdb_suffix _model_0.pdb --sampling_temp 0.2 --output_dir ./antibmpnn_output/bn0.5_raw_t0.2
+sbatch -p yejin run_antibmpnn_batch.sh --backbone_noise 0.5 --pdb_suffix _model_0.pdb --sampling_temp 0.4 --output_dir ./antibmpnn_output/bn0.5_raw_t0.4
+sbatch -p yejin run_antibmpnn_batch.sh --backbone_noise 0.5 --relaxed --sampling_temp 0.2 --output_dir ./antibmpnn_output/bn0.5_relaxed_t0.2
+sbatch -p yejin run_antibmpnn_batch.sh --backbone_noise 0.5 --relaxed --sampling_temp 0.4 --output_dir ./antibmpnn_output/bn0.5_relaxed_t0.4
