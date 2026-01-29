1330d2aad02abf20814fa680e8e489afb8fafc68
haokai-jojo <haokai@biomap.com>
2026-01-29 17:50:45 +0800

    check mfdesign

--- AntiBMPNN/launch.sh
+++ AntiBMPNN/launch.sh
@@ -8,8 +8,8 @@ sbatch -p yejin run_antibmpnn_batch.sh --backbone_noise 0.5 --relaxed --sampling
 sbatch -p yejin run_antibmpnn_batch.sh --backbone_noise 0.5 --relaxed --sampling_temp 0.4 --output_dir ./antibmpnn_output/bn0.5_relaxed_t0.4
 
 
-sbatch -p yejin-lo run_antibmpnn_batch.sh --backbone_noise 0.0 --pdb_suffix _model_0.pdb --sampling_temp 0.2 --output_dir ./antibmpnn_output/bn0.0_raw_t0.2
-sbatch -p yejin-lo run_antibmpnn_batch.sh --pdb_dir ../mfdesign_test_bagel_protein_boltz_base/predictions --backbone_noise 0.0 --pdb_suffix _model_0.pdb --sampling_temp 0.2 --output_dir ./antibmpnn_mfdesign_output/bn0.0_raw_t0.2
+sbatch -p yejin-lo run_antibmpnn_batch.sh --pdb_dir ../gen_trial_0/predictions --backbone_noise 0.0 --pdb_suffix _model_0.pdb --sampling_temp 0.2 --output_dir ./v2_antibmpnn_output/bn0.0_raw_t0.2
+sbatch -p yejin-lo run_antibmpnn_batch.sh --pdb_dir ../mfdesign_test_bagel_protein_boltz_base/predictions --backbone_noise 0.0 --pdb_suffix _model_0.pdb --sampling_temp 0.2 --output_dir ./v2_antibmpnn_mfdesign_output/bn0.0_raw_t0.2
 
 
 sbatch -p yejin run_antibmpnn_batch.sh --pdb_dir ../mfdesign_test_bagel_protein_boltz_base/predictions --backbone_noise 0.1 --pdb_suffix _model_0.pdb --sampling_temp 0.2 --output_dir ./antibmpnn_mfdesign_output/bn0.1_raw_t0.2
